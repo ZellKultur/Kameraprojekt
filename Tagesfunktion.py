545445b3f0013d66d68600190509ecd7624cd6ee
ZellKultur <80030717+ZellKultur@users.noreply.github.com>
2021-03-07 12:19:50 +0100

    Update Tagesfunktion.py

--- Tagesfunktion.py
+++ Tagesfunktion.py
@@ -6,6 +6,6 @@ camera = PiCamera()
 for i in range (5):
     camera.start_preview()
     sleep(2)
-    camera.capture('Home/pi/Desktop/image%s.jpg' % i)
+    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
     camera.stop_preview()
     sleep(10)
