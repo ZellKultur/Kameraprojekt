5baeae5a1cf622ad2d64d42512c90da28e02ed6e
ZellKultur <Tordsch23@web.de>
2021-03-07 09:57:29 +0100

    Preview in Loop

--- Tagesfunktion.py
+++ Tagesfunktion.py
@@ -3,9 +3,9 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.start_preview()
 for i in range (5):
+    camera.start_preview()
     sleep(2)
     camera.capture('Home/pi/Desktop/image%s.jpg' % i)
-
     camera.stop_preview()
+    sleep(10)
