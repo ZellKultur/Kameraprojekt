1aaae781174c130b0d03d7c6c663577033604937
TMoeser-Goe <58635904+TMoeser-Goe@users.noreply.github.com>
2021-05-09 09:28:38 +0200

    Removed colon from timestamp
    For compatibility reasons, the timestamps colons have been replaced by underscores.

--- Tagesfunktion.py
+++ Tagesfunktion.py
@@ -8,7 +8,10 @@ for i in range (96):
     start = time.time()                                                                                              #starpunkt Messung mit Uhrzeit
     camera.start_preview()
     time.sleep(2)
-    camera.capture('/home/pi/Desktop/Testbilder/image%s.jpg' % str(datetime.datetime.now().isoformat()))
+    timestamp = datetime.datetime.now().isoformat()
+    timestamp_without_colon = "".join([c if not c == ":" else "_" for c in timestamp])
+    filename = f'/home/pi/Desktop/Testbilder/image{timestamp_without_colon}.jpg'
+    camera.capture( % str())
     camera.stop_preview()
     time_needed = time.time() - start                                                                                # wieviel Zeit wurde benötigt? - wird abgezogen
     time.sleep(10 - time_needed)                                                                                     # benötigte Zeit von Loop abziehen
