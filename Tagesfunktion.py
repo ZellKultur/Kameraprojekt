b20a4af99ad0b725441551b128ea4737636f9de0
TMoeser-Goe <58635904+TMoeser-Goe@users.noreply.github.com>
2021-05-09 09:29:55 +0200

    Fixed error in Line 14
    Fixed error introduced in last commit due to forgetting to actually give the filename to camera.capture()

--- Tagesfunktion.py
+++ Tagesfunktion.py
@@ -11,7 +11,7 @@ for i in range (96):
     timestamp = datetime.datetime.now().isoformat()
     timestamp_without_colon = "".join([c if not c == ":" else "_" for c in timestamp])
     filename = f'/home/pi/Desktop/Testbilder/image{timestamp_without_colon}.jpg'
-    camera.capture( % str())
+    camera.capture(filename)
     camera.stop_preview()
     time_needed = time.time() - start                                                                                # wieviel Zeit wurde benötigt? - wird abgezogen
     time.sleep(10 - time_needed)                                                                                     # benötigte Zeit von Loop abziehen
